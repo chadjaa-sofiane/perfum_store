c175c380e1d46e8d8a1412bf2ae034beacb0309d
chadjaa sofiane <sofianne012@gmail.com>
2022-08-02 19:57:13 +0100

    add the image sources to the shop data

--- client/src/utils/data/shop.ts
+++ client/src/utils/data/shop.ts
@@ -1,3 +1,9 @@
+export type Ingredients = {
+  topNotes?: string[],
+  heartNotes?: string[],
+  backgroundNotes?: string[]
+}
+
 export type Product = {
   id: number;
   name: string;
@@ -8,11 +14,7 @@ export type Product = {
   description: string;
   category: string;
   available: boolean;
-  ingredients?: {
-    topNotes?: string[],
-    heartNotes?: string[],
-    backgroundNotes?: string[]
-  };
+  ingredients?: Ingredients;
   [key: string]: any;
 };
 
@@ -24,7 +26,7 @@ export const data: Product[] = [
     creator: "Sergey Borisov",
     price: 200,
     size: "100ml",
-    image: "https://picsum.photos/200/300",
+    image: "/parfums/atractone_Musk.jpg",
     count: 1,
     category: "perfume",
     description: "A silky, fresh and delicate floral-musky fragrance, Atractone Musk is promoted under the slogan Feel the attractionIts freshness begins with ozonated aldehyde freshness and pink pepper, backed by the moist aquatic freshness of a floral bouquet. Fresh spring flowers, lily of the valley, cyclamen and lilac, create a radiant floral cloud around their mistress, penetrated by the rays of the sun. A soft and enveloping oriental trail of cashmeran, amber, sandalwood and a seductive musky accord complete the composition.",
@@ -43,7 +45,7 @@ export const data: Product[] = [
     size: "100ml",
     count: 1,
     category: "perfume",
-    image: "https://picsum.photos/200/300",
+    image: "/parfums/dolceGabbana.jpg",
     description: "The One for Men Eau de Parfum by Dolce&Gabbana is a Woody Spicy fragrance for men. The One for Men Eau de Parfum was launched in 2015. The nose behind this fragrance is Olivier Polge. Top notes are grapefruit, coriander and basil; middle notes are ginger, cardamom and orange blossom; base notes are amber, tobacco and cedar.",
     ingredients: {
       topNotes: ["Grapefruit", "Coriander", "Basil"],
@@ -60,7 +62,7 @@ export const data: Product[] = [
     size: "100ml",
     count: 1,
     category: "perfume",
-    image: "https://picsum.photos/200/300",
+    image: "/parfums/103TiareFlower.jpg",
     description: "103 Tiare Flower, Jasmine, Hibiscus by Bon Parfumeur is a Floral fragrance for women and men. 103 tiare flower, jasmine, hibiscus was launched in 2017. The nose behind this fragrance is Karine Dubreuil- Sereni.Top notes are hibiscus, broom and bergamot; middle notes are tiare flower, jasmine sambac, frangipani and neroli; base notes are vanilla, musk and sand.",
     ingredients: {
       topNotes: ["Hibiscus", "Broom", "Bergamot"],
@@ -77,7 +79,7 @@ export const data: Product[] = [
     size: "100ml",
     count: 1,
     category: "perfume",
-    image: "https://picsum.photos/200/300",
+    image: "/parfums/l'InterditGivenchy.jpg",
     description: "L'Interdit Eau de Parfum by Givenchy is an Amber Floral fragrance for women. L'Interdit Eau de Parfum was launched in 2018. L'Interdit Eau de Parfum was created by Dominique Ropion, Anne Flipo and Fanny Bal. Top notes are pear and bergamot; middle notes are tuberose, orange blossom and jasmine sambac; base notes are vanilla, patchouli, ambroxan and vetiver.",
     ingredients: {
       topNotes: ["Pear", "Bergamot"],
@@ -94,7 +96,7 @@ export const data: Product[] = [
     size: "100ml",
     count: 1,
     category: "pefume",
-    image: "https://picsum.photos/200/300",
+    image: "/parfums/wildJardins.jpg",
     description: "Wilde by Jardins d'Ecrivains is a Aromatic Fruity fragrance for women and men. Wilde was launched in 2013. The nose behind this fragrance is Anais Biguine. Top notes are grapes and bergamot; middle notes are fig, carnation and tea; base notes are vetiver and oakmoss.",
     ingredients: {
       topNotes: ["Grapes", "Bergamot"],
@@ -111,27 +113,11 @@ export const data: Product[] = [
     size: "100ml",
     count: 1,
     category: "perfume",
-    image: "https://picsum.photos/200/300",
+    image: "/parfums/hermessenceCedre.jpg",
     description: "Hermessence Cedre Sambac by Hermès is a Woody Amber fragrance for women and men. Hermessence Cedre Sambac was launched in 2018. The nose behind this fragrance is Christine Nagel.",
     // guess the ingredients 
     ingredients: {
       topNotes: ["Jasam", "Sambacnnman", "manrere"]
-    },
-    available: true,
-  }, {
-    id: 7,
-    name: "Tobacco Oud Tom Ford for women and men",
-    creator: "Olivier Gillotin",
-    price: 150,
-    size: "100ml",
-    count: 1,
-    category: "pefume",
-    image: "https://picsum.photos/200/300",  
-    description: "Tobacco Oud by Tom Ford is a Woody Spicy fragrance for women and men. Tobacco Oud was launched in 2013. The nose behind this fragrance is Olivier Gillotin. Top note is whisky; middle notes are spicy notes, cinnamon and coriander; base notes are tobacco, agarwood (oud), sandalwood, incense, patchouli, benzoin, vanilla and cedar.",
-    ingredients: {
-      topNotes: ["Whisky", "Spicy Notes", "Cinnamon", "Coriander"],
-      heartNotes: ["Tobacco", "Agarwood", "Sandalwood", "Incense", "Patchouli", "Benzoin", "Vanilla", "Cedar"],
-      backgroundNotes: ["Tobacco", "Agarwood", "Sandalwood", "Incense", "Patchouli", "Benzoin", "Vanilla", "Cedar"]
     },
     available: true,
   }
