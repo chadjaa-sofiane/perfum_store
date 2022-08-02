c0936d557df8917ebb6dae97476e92bcc32ff6a5
chadjaa sofiane <sofianne012@gmail.com>
2022-08-02 20:03:28 +0100

    replace Navigate with use

--- client/src/pages/Cart/CartItemCard.tsx
+++ client/src/pages/Cart/CartItemCard.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/components/ProductButtons";
 import { State } from "@/store/store";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Price } from "../Shop/Product/Price";
 
 interface ICartItemCardProps {
@@ -12,18 +13,27 @@ interface ICartItemCardProps {
 }
 
 export const CartItemCard = ({ id }: ICartItemCardProps) => {
+  const navigate = useNavigate();
+
   const product = useSelector((state: State) =>
     state.shop.products.find((product) => product.id === id)
   );
+
+  const goToProductDrawwer = () => {
+    navigate(`/shop/${id}`);
+  };
+
   if (!product) return null;
   return (
-    <div className="flex flex-col sm:flex-row gap-4 w-full md:w-3/4 shadow-md">
+    <div
+      className="flex flex-col sm:flex-row gap-4 w-full md:w-3/4 shadow-md cursor-pointer"
+      onClick={goToProductDrawwer}
+    >
       <div className="flex-1 h-72">
         <img className="w-full h-full" src={product.image} alt={product.name} />
       </div>
       <div className="flex-1 flex flex-col p-4 gap-4">
         <Title3> {product.name} </Title3>
-        <Text> {product.description} </Text>
         <Price amount={product.price} />
         <div className="mt-auto flex gap-4">
           <RemoveFromCartButton id={id} />
